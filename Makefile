131fbef1087b565200504d1b8769cbed5b356523
Sebastian Freundt <freundt@ga-group.nl>
2012-12-04 08:10:27 +0000

    minor, allow multiple dirs to be watched at the same time
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ watchdir: watchdir-clo.c watchdir.o
 .SUFFIXES: .c .o .h .ggo
 
 .c.o:
-	$(CC) $(CFLAGS) -c -o $@ $< -DVERSION='"0.0"'
+	$(CC) $(CFLAGS) -c -o $@ $< -DVERSION='"0.0"' -D_POSIX_C_SOURCE=200112L -D_BSD_SOURCE
 
 .ggo.c:
 	gengetopt -F $* -i $<
--- watchdir.c
+++ watchdir.c
@@ -1,52 +1,118 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
 #include <sys/inotify.h>
 #include <sys/epoll.h>
 
+static void
+fputt(FILE *where)
+{
+	static char buf[64];
+	struct timeval tv[1];
+	struct tm *tm;
+	char *p = buf;
+
+#define R(p)	(sizeof(buf) - ((p) - buf))
+	(void)gettimeofday(tv, NULL);
+	tm = gmtime(&tv->tv_sec);
+	p += strftime(p, R(p), "%FT%T", tm);
+	snprintf(p, R(p), ".%06lu", tv->tv_usec);
+	fputs(buf, where);
+#undef R
+	return;
+}
+
+static int
+got_beef(int fd, const char *const *dirs)
+{
+	static uint8_t buf[4096];
+	struct inotify_event *restrict inev = (void*)buf;
+
+	if (read(fd, buf, sizeof(buf)) < sizeof(*inev)) {
+		return -1;
+	}
+
+	/* prepend time stamp */
+	fputt(stdout);
+	fputc('\t', stdout);
+	fputs(dirs[inev->wd - 1], stdout);
+	fputc('\t', stdout);
+	if (inev->mask & IN_CREATE || inev->mask & IN_MOVED_TO) {
+		fputc('c', stdout);
+	}
+	if (inev->mask & IN_MOVED_TO) {
+		fputc('i', stdout);
+	}
+	if (inev->mask & IN_DELETE || inev->mask & IN_MOVED_FROM) {
+		fputc('x', stdout);
+	}
+	if (inev->mask & IN_MOVED_FROM) {
+		fputc('o', stdout);
+	}
+	if (inev->len > 0) {
+		fputc('\t', stdout);
+		fputs(inev->name, stdout);
+	}
+	fputc('\n', stdout);
+	return 0;
+}
+
+static int
+watchdir(int inofd, const char *dir)
+{
+	int dirfd = inotify_add_watch(
+		inofd, dir,
+		IN_CREATE | IN_DELETE |
+		IN_MOVED_FROM | IN_MOVED_TO);
+	return dirfd;
+}
+
+
 #include "watchdir-clo.h"
 #include "watchdir-clo.c"
 
 int
 main(int argc, char *argv[])
 {
-	int inofd = inotify_init();
-	int dirfd = inotify_add_watch(
-		inofd, argv[1],
-		IN_CREATE | IN_DELETE | IN_MOVED_FROM | IN_MOVED_TO);
-	int epfd = epoll_create1(EPOLL_CLOEXEC);
+	struct wd_args_info argi[1];
 	struct epoll_event ev[1];
+	int inofd;
+	int epfd;
 
+	if (wd_parser(argc, argv, argi)) {
+		return 1;
+	} else if (argi->help_given) {
+		wd_parser_print_help();
+		return 0;
+	} else if (argi->inputs_num == 0) {
+		wd_parser_print_help();
+		return 1;
+	}
+
+	/* prepare watches */
+	inofd = inotify_init1(IN_CLOEXEC);
+	for (unsigned int i = 0; i < argi->inputs_num; i++) {
+		watchdir(inofd, argi->inputs[i]);
+	}
+
+	/* prepare main loop */
+	epfd = epoll_create1(EPOLL_CLOEXEC);
 	ev->events = EPOLLIN | EPOLLET;
 	ev->data.fd = inofd;
 	epoll_ctl(epfd, EPOLL_CTL_ADD, inofd, ev);
 
+	/* main loop */
 	while (epoll_wait(epfd, ev, 1, -1)) {
-		static uint8_t buf[4096];
-		struct inotify_event *inev = (void*)buf;
-
-		if (read(ev->data.fd, buf, sizeof(buf)) < sizeof(*inev)) {
+		if (got_beef(ev->data.fd, argi->inputs) < 0) {
 			break;
 		}
-		fputc('\t', stdout);
-		if (inev->mask & IN_CREATE || inev->mask & IN_MOVED_TO) {
-			fputc('c', stdout);
-		}
-		if (inev->mask & IN_DELETE || inev->mask & IN_MOVED_FROM) {
-			fputc('x', stdout);
-		}
-		if (inev->len > 0) {
-			fputc('\t', stdout);
-			fputs(argv[1], stdout);
-			fputc('/', stdout);
-			fputs(inev->name, stdout);
-		}
-		fputc('\n', stdout);
 	}
 
-	epoll_ctl(epfd, EPOLL_CTL_DEL, dirfd, ev);
+	/* clean up */
+	epoll_ctl(epfd, EPOLL_CTL_DEL, inofd, ev);
 	close(epfd);
-	inotify_rm_watch(inofd, dirfd);
 	close(inofd);
 	return 0;
 }
